7e0efb8722faffd5afadc0446acfefc7342958bb
Sabrina Jones <sabrina.jones87@gmail.com>
2019-07-13 19:53:22 +0000

    changes for console to work

--- lib/worst_offenders/review.rb
+++ lib/worst_offenders/review.rb
@@ -6,6 +6,16 @@ class WorstOffenders::Review
      1. first review
      2. second review
     DOC
+
+   review_1 = self.new
+   review_1.heading  = "Best car shop"
+   review_1.name = "Sue Brown"
+
+   review_2 = self.new
+   review_2.heading = "No other like it!"
+   review_2.name = "Bill Jones"
+
+   [review_1, review_2]
   end
 
 end
