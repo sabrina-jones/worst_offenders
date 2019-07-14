bbb55701de3150de003c2823d1f36a1a51af0430
Sabrina Jones <sabrina.jones87@gmail.com>
2019-07-14 15:32:37 +0000

    pagination

--- lib/worst_offenders/review.rb
+++ lib/worst_offenders/review.rb
@@ -18,24 +18,35 @@ class WorstOffenders::Review
   def self.scrape
 
     doc = Nokogiri::HTML(open("https://www.dealerrater.com/dealer/McKaig-Chevrolet-Buick-A-Dealer-For-The-People-review-23685/"))
-    
+
     offender_reviews = Array.new
-    reviews = doc.css("div.review-entry") #data for all reviews on the page 
-    
+    reviews = doc.css("div.review-entry") #data for all reviews on the page
 
-    reviews.each do |review|
-      
-       offender_review = {
-    
-        heading: review.css("h3.no-format").text.delete('\"'),
-        user:    review.css("span.italic").text.delete('- '),
-        content: review.css("p.font-16.review-content").text
-      }
+    page = 1
+    last_page = 5
 
-      offender_reviews << offender_review
-    
-    end
+   while page <= last_page
+      pagination_url = "https://www.dealerrater.com/dealer/McKaig-Chevrolet-Buick-A-Dealer-For-The-People-review-23685/page#{page}/"
+      pagination_doc = Nokogiri::HTML(open(pagination_url))
+      pagination_reviews = pagination_doc.css("div.review-entry")
+
+      puts pagination_url
+
+      pagination_reviews.each do |review|
+
+          offender_review = {
+
+            heading: review.css("h3.no-format").text.delete('\"'),
+            user:    review.css("span.italic").text.delete('- '),
+            content: review.css("p.font-16.review-content").text
+          }
+
+          offender_reviews << offender_review
+          puts "Added #{offender_review[:heading]}"
+        end
+        page += 1
+     end
       binding.pry
-  end 
+  end
 
 end
